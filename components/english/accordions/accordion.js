966efbd3543269864ac230c7f2a7877ca3dc4f42
kantiman <krantitherevolution99@gmail.com>
2021-01-31 10:36:12 +0545

    changes

--- components/english/accordions/accordion.js
+++ components/english/accordions/accordion.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { Data } from './data';
-import { IconContext } from 'react-icons';
-import { FiPlus, FiMinus } from 'react-icons/fi';
 
 const Accordion = () => {
   const [clicked, setClicked] = useState(false);
@@ -16,27 +14,69 @@ const Accordion = () => {
   };
 
   return (
-    <IconContext.Provider value={{ color: '#00FFB9', size: '25px' }}>
-      <div class="AccordionSection">
-        <div class="faq_container">
-          {Data.map((item, index) => {
-            return (
-              <>
-                <div class="Wrap" onClick={() => toggle(index)} key={index}>
-                  <h1>{item.question}</h1>
-                  <span>{clicked === index ? <FiMinus /> : <FiPlus />}</span>
-                </div>
-                {clicked === index ? (
-                  <div class="Dropdown">
-                    <p>{item.answer}</p>
-                  </div>
-                ) : null}
-              </>
-            );
-          })}
-        </div>
-      </div>
-    </IconContext.Provider>
+      // <div class="AccordionSection">
+      //   <div class="faq_container">
+      //     {Data.map((item, index) => {
+      //       return (
+      //         <>
+      //           <div class="Wrap" onClick={() => toggle(index)} key={index}>
+      //             <h1>{item.question}</h1>
+      //             <span>{clicked === index ? <FiMinus /> : <FiPlus />}</span>
+      //           </div>
+      //           {clicked === index ? (
+      //             <div class="Dropdown">
+      //               <p>{item.answer}</p>
+      //             </div>
+      //           ) : null}
+      //         </>
+      //       );
+      //     })}
+      //   </div>
+      // </div>
+
+                    <div class="faq-main-box">
+                      <div class="faq-div-holder in">
+                        <div class="faq-holder">
+
+                            {Data.map((item, index) => {
+                              return (
+                                <>
+                                      <div class="faq-item" onClick={() => toggle(index)} key={index}>
+                                          <div class="faq-title">
+                                              <h6>{item.question} </h6>
+                                          </div>
+                                          {clicked === index ? (
+                                            <div class="faq-description">
+                                                <p>{item.answer}</p>
+                                            </div>
+                                          ) : null}
+                                      </div>
+                                  
+                                </>
+                              );
+                           })}
+                           </div>
+
+                           <div class="quick-contact">
+                              <div class="quick-contact-holder">
+                                  <div class="quick-contact-icon"><img src="img/quick-contact.svg" alt="" /></div>
+                                  <div class="quick-contact-title">
+                                      <h6>Call Us anytime</h6>
+                                  </div>
+                                  <div class="quick-contact-des">
+                                      <p>نحن متاحين للإجابة على كل <br />استفساراتك و اسئلتك</p>
+                                  </div>
+                                  <div class="quick-contact-list">
+                                      <ul>
+                                          <li><a href="#" title="title">+966(5) 587-3407</a></li>
+                                          <li><a href="#" title="title">+966(5) 587-3407</a></li>
+                                      </ul>
+                                  </div>
+                              </div>
+                          </div>
+                        </div>
+                    </div>
+
   );
 };
 
--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,8 @@
 import Header from '../components/english/header'
 import Footer from '../components/english/footer'
 import Accordion from '../components/english/accordions/accordion'
+import Link from 'next/link'
+
 
 function contact_us() {
     return ( 
@@ -19,90 +21,12 @@ function contact_us() {
                                     <p class="colorange">Most of what was inquired about in Bonat</p>
                                 </div>
                             </div>
-                            <div class="faq-main-box">
-                                <div class="faq-div-holder">
-                                    <div class="faq-holder">
-                                        <div class="faq-item">
-                                            <div class="faq-title">
-                                                <h6>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam </h6>
-                                            </div>
-                                            <div class="faq-description">
-                                                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                                                    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-                                                    vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-                                                    no sea takimata sanctus est Lorem</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="faq-div-holder in">
-                                    <div class="faq-holder">
-                                        <div class="faq-item">
-                                            <div class="faq-title">
-                                                <h6>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam </h6>
-                                            </div>
-                                            <div class="faq-description">
-                                                <p>Please Fill The Text</p>
-                                            </div>
-                                        </div>
-                                        <div class="faq-item">
-                                            <div class="faq-title">
-                                                <h6>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam </h6>
-                                            </div>
-                                            <div class="faq-description">
-                                                <p>Please Fill The Text</p>
-                                            </div>
-                                        </div>
-                                        <div class="faq-item">
-                                            <div class="faq-title">
-                                                <h6>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam </h6>
-                                            </div>
-                                            <div class="faq-description">
-                                                <p>Please Fill The Text</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="quick-contact">
-                                        <div class="quick-contact-holder">
-                                            <div class="quick-contact-icon"><img src="img/quick-contact.svg" alt="" /></div>
-                                            <div class="quick-contact-title">
-                                                <h6>Call Us anytime</h6>
-                                            </div>
-                                            <div class="quick-contact-des">
-                                                <p>نحن متاحين للإجابة على كل <br />استفساراتك و اسئلتك</p>
-                                            </div>
-                                            <div class="quick-contact-list">
-                                                <ul>
-                                                    <li><a href="#" title="title">+966(5) 587-3407</a></li>
-                                                    <li><a href="#" title="title">+966(5) 587-3407</a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="faq-div-holder">
-                                    <div class="faq-holder">
-                                        <div class="faq-item">
-                                            <div class="faq-title">
-                                                <h6>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam </h6>
-                                            </div>
-                                            <div class="faq-description">
-                                                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                                                    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-                                                    vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-                                                    no sea takimata sanctus est Lorem</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="faq-button-box"><a class="btn orange" href="#" title="title">Send Message</a></div>
+                            <Accordion />
+                            <div class="faq-button-box"><Link href="../../contact_us"><a class="btn orange" title="title">Send Message</a></Link></div>
                         </div>
                     </div>
             </section>
     {/* <!-- FAQ SECTION ENDS-->                          */}
-    <Accordion />
-
             <Footer />
         </div>
         )
